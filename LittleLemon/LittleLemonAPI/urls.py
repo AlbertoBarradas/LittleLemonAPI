dfc1dfe77d4eadf7139a6d26f35a3175d1bf71a1
AlbertoBarradas <sebastianbago@hotmail.com>
2025-08-08 17:17:44 -0600

    Added features to retrieve items and to add users to manager group

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('menu-items/', views.MenuItemsView.as_view()),
-    path('category/', views.CategoryView.as_view())
+    path('menu-items/', views.MenuItemsListCreate.as_view()),
+    path('menu-items/<int:pk>/', views.MenuItemsRetrieve.as_view()),
+    path('category/', views.CategoryCreate.as_view()),
+    path('groups/manager/users/', views.managers),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Category, MenuItem, Order, OrderItem, Cart
-from rest_framework import generics, status, viewsets
-from django.contrib.auth.models import User
+from rest_framework import generics, status, viewsets, filters
+from django.contrib.auth.models import User, Group
 from .serializers import MenuItemSerializer, CategorySerializer
 from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 # Create your views here.
-class MenuItemsView(generics.ListCreateAPIView):
+class MenuItemsListCreate(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'category']
+
+class CategoryCreate(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class MenuItemsRetrieve(generics.RetrieveAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
-class CategoryView(generics.CreateAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def managers(request):
+    username = request.data['username']
+    if username:
+        user = get_object_or_404(User, username=username)
+        manager = Group.objects.get(name="Manager")
+        if request.method == 'POST':
+            manager.user_set.add(user)
+        elif request.method == 'DELETE':
+            manager.user_set.remove(user)
+        return Response({"message":"user added to manager group"})
+    return Response({"message":"error"}, status.HTTP_400_BAD_REQUEST)
